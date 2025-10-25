b01a61280be4d22ae1777471d2d284fd22116e7c
Jacobo Arroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-10-24 22:18:30 -0500

    fix:Estandarizacion del protocolo de cargar las ciudades

--- start/routes/Cities.ts
+++ start/routes/Cities.ts
@@ -1,5 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
-    Route.get("/charge", "CitiesController.chargeCities");
+    Route.post("/charge", "CitiesController.chargeCities");
     Route.get("/find", "CitiesController.find");
 }).prefix("/cities");
