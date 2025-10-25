31037a3aad8c4328340b7fbf7dc793b5d3255c83
Jacobo Arroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-10-24 22:10:12 -0500

    feat:Se implementa metodo en el controlador de ciudades  para traer las ciudades de un departamento

--- app/Controllers/Http/CitiesController.ts
+++ app/Controllers/Http/CitiesController.ts
@@ -3,7 +3,7 @@ import City from 'App/Models/City';
 import axios from 'axios'
 
 export default class CitiesController {
-    public async chargeCities({response }: HttpContextContract) {
+    public async chargeCities({ response }: HttpContextContract) {
         try {
             const responseApi = await axios.get('https://api-colombia.com/api/v1/City?sortBy=name&sortDirection=asc')
             const cities = responseApi.data;
@@ -16,6 +16,16 @@ export default class CitiesController {
             response.status(500).send('Error loading cities');
         }
     }
-    public async getCities({ }: HttpContextContract) {
+    public async find({ params, response }: HttpContextContract) {
+        if (!params.idDepartment) {
+            return response.status(400).send('Department ID is required');
+        }
+        try {
+            const cities = await City.query().where('department_id', params.idDepartment);
+            return response.status(200).send(cities);
+        } catch (error) {
+            response.status(500).send('Error fetching cities');
+            console.log(error);
+        }
     }
 }
