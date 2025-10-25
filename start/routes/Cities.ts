5a8d1b48e05049b921e4c9f5bc9becbeed2edfdf
Jacobo Arroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-10-24 22:11:32 -0500

    feat(CitiesRouter):Se agrega ruta para el metodo find

--- start/routes/Cities.ts
+++ start/routes/Cities.ts
@@ -1,4 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
     Route.get("/charge", "CitiesController.chargeCities");
+    Route.get("/find", "CitiesController.find");
 }).prefix("/cities");
