9e889faed911a6527c1f6a97093befa12682fb75
Jacobo Arroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-10-24 22:31:42 -0500

    feat(City):Se implementa relacion con trayectos

--- app/Models/City.ts
+++ app/Models/City.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
 import Department from './Department'
+import Journey from './Journey'
 
 export default class City extends BaseModel {
   @column({ isPrimary: true })
@@ -9,13 +10,20 @@ export default class City extends BaseModel {
   public name: string
   @column()
   public department_id: number
+
   @belongsTo(() => Department,
     { foreignKey: 'department_id' })
   public department: BelongsTo<typeof Department>
 
+  @hasMany(() => Journey, { foreignKey: 'origin_id' })
+  public originJourneys: HasMany<typeof Journey>
+
+  @hasMany(() => Journey, { foreignKey: 'destination_id' })
+  public destinationJourneys: HasMany<typeof Journey>
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
-
+  
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
 }
