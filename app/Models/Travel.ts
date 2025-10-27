ccdb7c6fecca2878697abead551e8a5da09e8381
lrivera40256 <luis.rivera40256@ucaldas.edu.co>
2025-10-26 19:57:17 -0500

    Se implementan relaciones entre modelos hasMany, belongsTo,

--- app/Models/Travel.ts
+++ app/Models/Travel.ts
@@ -1,13 +1,43 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Quota from './Quota'
+import TravelCustomer from './TravelCustomer'
 
 export default class Travel extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public description: string
+
+  @column.date()
+  public creationDate: DateTime
+
+  @column.date()
+  public startDate: DateTime
+
+  @column.date()
+  public endDate: DateTime
+
+  @column()
+  public price: number
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasMany(() => Quota, {
+    foreignKey: 'travel_id'
+  })
+  public quotas: HasMany<typeof Quota>
+
+  @hasMany(() => TravelCustomer, {
+    foreignKey: 'travel_id'
+  })
+  public travelCustomers: HasMany<typeof TravelCustomer>
 }
