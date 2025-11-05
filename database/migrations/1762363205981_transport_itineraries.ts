edc635db58a229081f17f5bce789f9470bdbf96f
lrivera40256 <luis.rivera40256@ucaldas.edu.co>
2025-11-05 12:25:56 -0500

    Se crea migracion de transportyItinerary

---
+++ database/migrations/1762363205981_transport_itineraries.ts
@@ -0,0 +1,33 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'transport_itineraries'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('sequence').notNullable()
+      table.integer('travel_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('travels')
+        .onDelete('CASCADE')
+      table.integer('journey_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('journeys')
+        .onDelete('CASCADE')
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
