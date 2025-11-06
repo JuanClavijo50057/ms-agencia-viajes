25a7163d9905a791a3b9a355378ded44cd874dd4
Jacobo Arroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-11-05 19:40:10 -0500

    feat:(ServiceTransportation):Se el modelo

---
+++ app/Models/ServiceTransportation.ts
@@ -0,0 +1,46 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Vehicle from './Vehicle'
+import Journey from './Journey'
+import TransportItinerary from './TransportItinerary'
+
+export default class ServiceTransportation extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column.date()
+  public startDate: DateTime
+
+  @column.date()
+  public endDate: DateTime
+
+  @column()
+  public cost: number
+
+  @column()
+  public transportation_id: number
+
+  @column()
+  public journey_id: number
+
+  @belongsTo(() => Vehicle, {
+    foreignKey: 'vehicle_id',
+  })
+  public vehicle: BelongsTo<typeof Vehicle>
+
+  @belongsTo(() => Journey, {
+    foreignKey: 'journey_id',
+  })
+  public journey: BelongsTo<typeof Journey>
+
+  @hasMany(() => TransportItinerary, {
+    foreignKey: 'service_transportation_id',
+  })
+  public transportItineraries: HasMany<typeof TransportItinerary>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
