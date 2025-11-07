9dd2fab3e5045e09e7a80f08cf4a0750a31bc8c2
Jacobo Arroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-11-06 23:10:36 -0500

    feat(DepartamentRoute):Se agrega endoint para la obtencion de los departamentos

--- start/routes/Department.ts
+++ start/routes/Department.ts
@@ -1,4 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
     Route.post("/charge", "DepartmentsController.chargeDepartments");
+    Route.get("/", "DepartmentsController.findAll");
 }).prefix("/departments");
