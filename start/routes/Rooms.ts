73635be764b0ce4c1d315bc7574794da5aa1f443
Jacobo Arroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-11-10 15:21:47 -0500

    fix:Se elimina duplicida de rutas

--- start/routes/Rooms.ts
+++
@@ -1,5 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-Route.group(() => {
-    Route.post("", "RoomsController.create");
-    Route.get("/hotel/:hotelId", "RoomsController.findByHotel");
-}).prefix("/rooms");
