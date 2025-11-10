f3bc44014e683135fdd69bf5b9781ce1a2034c8a
Jacobo Arroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-11-09 21:53:45 -0500

    fix:correcion en la ruta de los departamentos

--- start/routes/Department.ts
+++ start/routes/Department.ts
@@ -2,4 +2,4 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
     Route.post("/charge", "DepartmentsController.chargeDepartments");
     Route.get("/", "DepartmentsController.findAll");
-}).prefix("/departments");
+}).prefix("/departaments");
