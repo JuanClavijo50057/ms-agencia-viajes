05b2d91ba440d840238bbac2aebf9dfa46d2ba25
Jacobo Arroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-11-10 14:47:16 -0500

    feat:Se agrega ruta de los departamentos

--- start/routes/Department.ts
+++ start/routes/Department.ts
@@ -2,4 +2,5 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
     Route.post("/charge", "DepartmentsController.chargeDepartments");
     Route.get("/", "DepartmentsController.findAll");
+    Route.get("/available-hotels", "DepartmentsController.findDepartmentsWithAvailableHotels");
 }).prefix("/departments");
