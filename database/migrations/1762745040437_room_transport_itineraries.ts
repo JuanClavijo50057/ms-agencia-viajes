db16c88fd4dca4c4455793d763bd912d9a0dd3e7
JuanClavijo50057 <juan.clavijo50057@ucaldas.edu.co>
2025-11-09 22:26:39 -0500

    implementacion relacion n a n faltante

---
+++ database/migrations/1762745040437_room_transport_itineraries.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'room_transport_itineraries'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('room_id').unsigned().notNullable()
+        .references('id').inTable('rooms').onDelete('CASCADE')
+      table.integer('transport_itinerary_id').unsigned().notNullable()
+        .references('id').inTable('transport_itineraries').onDelete('CASCADE')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
