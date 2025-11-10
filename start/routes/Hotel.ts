d9d179c15fe98d503dcd38c5cb5843cc87368e68
Jacobo Arroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-11-10 15:15:05 -0500

    fix:Se corrige enrutamiento de las habitaciones

--- start/routes/Hotel.ts
+++ start/routes/Hotel.ts
@@ -2,7 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
     Route.get('/', 'HotelsController.findAll')
-    Route.get('/city/:cityId', 'HotelsController.findByCity')
+    Route.get('/city/:cityId', 'HotelsController.getHotelsByCity')
     Route.get('/stars/:stars', 'HotelsController.findByStars')
     Route.post('/', 'HotelsController.create')
     Route.put('/:id', 'HotelsController.update')
