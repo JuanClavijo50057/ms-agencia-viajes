d782b0b171d9fb3579b3179611a5cba17fa83eef
Jacobo Arroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-12-07 15:26:53 -0500

    feat:se descomenta validacion de interceptor

--- app/Middleware/Security.ts
+++ app/Middleware/Security.ts
@@ -5,38 +5,39 @@ export default class Security {
   public async handle({ request, response }: HttpContextContract, next: () => Promise<void>) {
     let theRequest = request.toJSON()
     if (true) {
-    // if (theRequest.headers.authorization) {
-      // let token = theRequest.headers.authorization.replace("Bearer ", "")
-      // let thePermission: object = {
-      //   url: theRequest.url,
-      //   method: theRequest.method
-      // }
-      try {
-        // const result = await axios.post(`${Env.get('MS_SECURITY')}/api/public/security/permissions-validation`, thePermission,
-        //   {
-        //     headers: {
-        //       Authorization: `Bearer ${token}`
-        //     }
-        //   }
-        // )
-        // if (result.data == true) {
-        // } else {
-        //   return response.status(403)
-        // }
-        await next()
-      } catch (error) {
-        // Si es un error que viene del microservicio (tiene response), lo manejas
-        if (axios.isAxiosError(error) && error.response) {
-          const status = error.response.status || 403
-          return response.status(status)
+      if (theRequest.headers.authorization) {
+        let token = theRequest.headers.authorization.replace("Bearer ", "")
+        let thePermission: object = {
+          url: theRequest.url,
+          method: theRequest.method
         }
-        // Si no es un error HTTP del microservicio, lánzalo para que lo maneje el Handler global
-        throw error
+        try {
+          const result = await axios.post(`${Env.get('MS_SECURITY')}/api/public/security/permissions-validation`, thePermission,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            }
+          )
+          if (result.data == true) {
+          } else {
+            return response.status(403)
+          }
+          await next()
+        } catch (error) {
+          // Si es un error que viene del microservicio (tiene response), lo manejas
+          if (axios.isAxiosError(error) && error.response) {
+            const status = error.response.status || 403
+            return response.status(status)
+          }
+          // Si no es un error HTTP del microservicio, lánzalo para que lo maneje el Handler global
+          throw error
+        }
+
+      } else {
+        return response.status(401)
       }
 
-    } else {
-      return response.status(401)
     }
-
   }
 }
