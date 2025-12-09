e40efca108eb78eb158dc8b16a7374eb77fca859
lrivera40256 <luis.rivera40256@ucaldas.edu.co>
2025-12-09 18:02:43 -0500

    feat:Implement findTravelCustomersinPaymentOrPaid functionality

--- start/routes/TravelCustomer.ts
+++ start/routes/TravelCustomer.ts
@@ -4,4 +4,5 @@ Route.group(() => {
     Route.post("/", "TravelCustomersController.create");
     Route.patch("/:id", "TravelCustomersController.update");
     Route.delete("/:id", "TravelCustomersController.delete");
+    Route.get("/:userId/in-payment-or-paid", "TravelCustomersController.findTravelCustomersinPaymentOrPaid");
 }).prefix("/travel-customer");
