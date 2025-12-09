ddb8ce5a9acd812189ce95fc53757b479e1ec2c3
lrivera40256 <luis.rivera40256@ucaldas.edu.co>
2025-12-08 19:50:13 -0500

    feat:Enhance Security Middleware with logging and token handling

--- app/Middleware/Security.ts
+++ app/Middleware/Security.ts
@@ -1,11 +1,14 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import axios from 'axios';
 import Env from '@ioc:Adonis/Core/Env'
+import SecurityService from 'App/Services/SecurityService';
 export default class Security {
   public async handle({ request, response }: HttpContextContract, next: () => Promise<void>) {
     let theRequest = request.toJSON()
-    if (true) {
+    console.log('Middleware Security - Request URL:', theRequest.url);
       if (theRequest.headers.authorization) {
+        console.log('Authorization Header:', theRequest.headers.authorization);
+        SecurityService.token = theRequest.headers.authorization
         let token = theRequest.headers.authorization.replace("Bearer ", "")
         let thePermission: object = {
           url: theRequest.url,
@@ -27,17 +30,17 @@ export default class Security {
         } catch (error) {
           // Si es un error que viene del microservicio (tiene response), lo manejas
           if (axios.isAxiosError(error) && error.response) {
+            console.log('Error en Middleware Security - Axios Error:', error.message)
             const status = error.response.status || 403
             return response.status(status)
           }
           // Si no es un error HTTP del microservicio, lánzalo para que lo maneje el Handler global
+          console.log('Error en Middleware Security:', error)
           throw error
         }
 
       } else {
         return response.status(401)
       }
-
-    }
   }
 }
